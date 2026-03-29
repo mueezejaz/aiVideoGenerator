2ee09590829bd5078d2209e9f0f5c0d0c20753e5
mueezejaz <mueezejaz12345678@gmail.com>
2026-03-29 22:38:20 +0500

    add stuff for building project

--- src/windows/mainWindow.js
+++ src/windows/mainWindow.js
@@ -20,12 +20,13 @@ function createMainWindow() {
 
   // To do
   // const isDev = process.env.NODE_ENV === 'development'
-  const isDev = true;
+  const isDev = false;
   if (isDev) {
     win.loadURL('http://localhost:5173')
     win.webContents.openDevTools()
   } else {
-    win.loadFile(path.join(__dirname, '../../../src/client/dist/index.html'))
+    win.loadFile(path.join(__dirname, '../../src/client/dist/index.html'))
+    win.webContents.openDevTools()
   }
 
   win.once('ready-to-show', () => win.show())
